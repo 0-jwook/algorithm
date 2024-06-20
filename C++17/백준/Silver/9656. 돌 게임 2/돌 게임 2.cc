8d3b88ed5bca3486f8909cd6e02ae37d75ac9433
1301 공재욱 <140470684+My-destiny-ae@users.noreply.github.com>
2024-06-20 12:26:58 +0900

    [Silver V] Title: 돌 게임 2, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C++17/백준/Silver/9656. 돌 게임 2/돌 게임 2.cc
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    int a;
+    int b = 0;
+    scanf("%d", &a);
+    while (a>0) {
+        if (a>=3) {
+            a-=3;
+            b++;
+        }else if (3>a){
+            a-=1;
+            b++;
+        }
+    }
+    if (b%2 == 0) {
+        printf("SK");
+    }else{
+        printf("CY");
+    }
+    return 0;
+}
