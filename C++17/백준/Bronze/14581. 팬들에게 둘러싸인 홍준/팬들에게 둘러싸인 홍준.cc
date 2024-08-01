f6fde1342a4ece803e9ff651163899d94f78bc60
1301 공재욱 <140470684+wodnr3569@users.noreply.github.com>
2024-08-01 21:42:50 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+int main(void) {
+    char id[100];
+    scanf("%s", id);
+    printf(":fan::fan::fan:\n");
+    printf(":fan::%s::fan:\n", id);
+    printf(":fan::fan::fan:");
+    
+    return 0;
+}
