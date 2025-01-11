0d539d8ccabcd13b5c2aa02e30a152b59c43e674
공재욱 <140470684+0-jwook@users.noreply.github.com>
2025-01-11 15:24:20 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    int a;
+    int result = 0;
+    cin >> a;
+    result = a/11;
+    result *= 10;
+    cout<<result<<endl;
+    return 0;
+}
+
