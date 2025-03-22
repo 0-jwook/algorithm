f5407e2981e83ade1bb8bb6a758938c415f223f3
공재욱 <140470684+0-jwook@users.noreply.github.com>
2025-03-23 00:54:29 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int s;
+    int r;
+    cin >> r >> s;
+    cout << (s*2)-r << endl;
+    
+    
+    
+    return 0;
+}
